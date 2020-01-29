a2dda34e7e34415dddbf3d4aad68b40317386611
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2020-01-29 17:42:48 -0500

    Add new packages and update tests accordingly
    New packages added:
    BeautifulSoup4 - HTML and XML parser
    rope - Python refactoring tool (installed under development packages)

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,19 +1,20 @@
 import pytest
 
-# TODO: Possible functions/features to test:
-#       - Search results (feature test)
-
+from bs4 import BeautifulSoup
 
 # Test that the application successfully serves the main page
 def test_search_page(client):
     res = client.get("/")
     assert res.status_code == 200
-    assert b'Unofficial copyright search prototype' in res.data
+    page = BeautifulSoup(res.data, 'html.parser')
+    assert page.title.string == "Find Copyright Entries - CCE Search"
 
 # Test that a successful search yields results
 def test_search_functioning(client):
     # Search for a title that we know will result in numerous matches
     res = client.get("/?type=ft&term=I+Ching")
     assert res.status_code == 200
-    
-
+    page = BeautifulSoup(res.data, 'html.parser')
+    # print(page.prettify())
+    # print(page.title)
+    assert res.status_code == 200
