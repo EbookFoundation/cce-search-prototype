7aa8fbfe092f8cde1f36239177dc2b242fa68bd9
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2020-01-29 21:49:31 -0500

    Added tests for successful and unsuccessful registration number searches

--- tests/test_search.py
+++ tests/test_search.py
@@ -11,7 +11,7 @@ def test_search_page(client):
 
 # Test that a successful full text search yields results
 def test_successful_full_text_search(client):
-    # Search for a title that we know will result in numerous matches
+    # Search for a title that will result in numerous matches
     res = client.get("/?type=ft&term=I+Ching")
     assert res.status_code == 200
     # Search for the existence of <h2>Results</h2>
@@ -19,17 +19,36 @@ def test_successful_full_text_search(client):
     # TODO: Convert to more intelligent solution using beautiful soup?
     # page = BeautifulSoup(res.data, 'html.parser')
 
-# Test an unsuccessful full text search
+# Test that an unsuccessful full text search returns to the home screen
 def test_unsuccessful_full_text_search(client):
     # Search for a nonexistent title
     res = client.get("/?type=ft&term=blahblahblahblahblahblah")
+    assert res.status_code == 200
     result_page = BeautifulSoup(res.data, 'html.parser')
     home_res = client.get("/")
     home_page = BeautifulSoup(home_res.data, 'html.parser')
     assert result_page.string == home_page.string
 
-# TODO: test_successful_registration_number_search(client)
-# TODO: test_unsuccessful_registration_number_search(client)
+# Test that a successful registration number search yields results
+def test_successful_registration_number_search(client):
+    # Search for a registration number that will result in a match
+    res = client.get("/?type=reg&term=A45173")
+    assert res.status_code == 200
+    # Search for the existence of <h2>Results</h2>
+    assert b'<h2>Results</h2>' in res.data
+    # TODO: Convert to more intelligent solution using beautiful soup?
+    # page = BeautifulSoup(res.data, 'html.parser')
+
+# Test that an unsuccessful registration number search returns to the home screen
+def test_unsuccessful_registration_number_search(client):
+    # Search for a nonexistant registration number
+    res = client.get("/?type=reg&term=ThisIsNotARegistrationNumber")
+    assert res.status_code == 200
+    result_page = BeautifulSoup(res.data, 'html.parser')
+    home_res = client.get("/")
+    home_page = BeautifulSoup(home_res.data, 'html.parser')
+    assert result_page.string == home_page.string
+    
 # TODO: test_successful_renewal_number_search(client)
 # TODO: test_successful_renewal_number_search(client)
 
